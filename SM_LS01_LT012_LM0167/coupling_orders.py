713a820e7709a4810f302f5e0d24b2a515f715bf
Andrew Michael Levin <anlevin@t3btch110.mit.edu>
2015-02-05 02:06:32 -0500

    added a model with all of the dimension 8 operators that affect the WWWW vertex

---
+++ SM_LS01_LT012_LM0167/coupling_orders.py
@@ -0,0 +1,20 @@
+# This file was automatically created by FeynRules 2.0.20
+# Mathematica version: 9.0 for Microsoft Windows (64-bit) (January 25, 2013)
+# Date: Wed 12 Mar 2014 02:18:27
+
+
+from object_library import all_orders, CouplingOrder
+
+
+NP = CouplingOrder(name = 'NP',
+                   expansion_order = 99,
+                   hierarchy = 1)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 1)
+

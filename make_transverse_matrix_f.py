f8a77915fb63e4a72f26ee4d181de9d426d36fac
Andrew Michael Levin <anlevin@t3btch110.mit.edu>
2015-02-10 07:39:28 -0500

    added a script to remove all of the W polarizations except the transverse polarization in a matrix*.f file

---
+++ make_transverse_matrix_f.py
@@ -0,0 +1,25 @@
+#!/bin/env python
+
+import subprocess, sys, os, re, math
+
+inputfilename=sys.argv[1]
+outputfilename=sys.argv[2]
+
+input_file = open(inputfilename)
+output_file = open(outputfilename, 'w')
+
+index=1
+
+for line in input_file:
+
+    if line == "      PARAMETER (             NCOMB=144)\n":
+        output_file.write("      PARAMETER (             NCOMB=128)\n")
+    elif "DATA (NHEL" in line:
+        if line.split(',')[5] != " 0" or line.split(',')[6] != " 0":
+            output_file.write(line.replace(line.split(',')[1],"  "+str(index)+")"))
+            index=index+1
+    else:
+        output_file.write(line)
+
+input_file.close()
+output_file.close()

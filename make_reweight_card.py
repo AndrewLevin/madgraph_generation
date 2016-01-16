8f8e2ee5fd827139a2c1d6468a61cee580fe99d8
Andrew <anlevin@cern.ch>
2016-01-16 11:32:08 -0500

    update the parameters ranges in the reweight card

--- make_reweight_card.py
+++ make_reweight_card.py
@@ -33,51 +33,51 @@ print "#* You can have as many weight as you want."
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 1 " +str(i*0.4) + "00000e-10"
+     print "        set anoinputs 1 " +str(i*0.5) + "00000e-11"
 
 #FS1
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 2 " +str(i/2.) + "00000e-10"
+     print "        set anoinputs 2 " +str(i*0.1) + "00000e-10"
 
 #FM0
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 3 " +str(i/2.) + "00000e-10"
+     print "        set anoinputs 3 " +str(i*0.5) + "00000e-11"
 
 #FM1
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 4 " +str(i/2.) + "00000e-10"
+     print "        set anoinputs 4 " +str(i*0.5) + "00000e-11"
 
 #FM6
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 9 " +str(i*1.) + "00000e-10"
+     print "        set anoinputs 9 " +str(i*0.5) + "00000e-11"
 
 #FM7
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 10 " +str(i*1.) + "00000e-10"     
+     print "        set anoinputs 10 " +str(i*0.1) + "00000e-10"     
 
 #FT0
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 11 " +str(i*0.5) + "00000e-11"
+     print "        set anoinputs 11 " +str(i*0.5) + "00000e-12"
 
 #FT1
 for i in range(-5,6):
      print "launch"
-     print "        set anoinputs 12 " +str(i*0.2) + "00000e-11"          
+     print "        set anoinputs 12 " +str(i*0.2) + "00000e-12"          
 
 #FT2
 for i in range(-5,6):
      print "launch"
      print "        set anoinputs 12 0.000000e+00"
-     print "        set anoinputs 13 " +str(i*0.5) + "00000e-11"
+     print "        set anoinputs 13 " +str(i*0.5) + "00000e-12"
